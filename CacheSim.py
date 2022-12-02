3a59b49d0c0501bd9570fb7e3d15343a4a816c20
johnglass2003 <50562358+johnglass2003@users.noreply.github.com>
2022-12-02 12:01:17 -0500

    Fully Associative Cache FIFO and LRU

--- CacheSim.py
+++ CacheSim.py
@@ -1,5 +1,5 @@
 import math
-
+#------------------------------------------------Table Line-------------------------------------------------------#
 class tableLine:
     def __init__(self, i, s, t, c):
         self.index = i
@@ -10,7 +10,7 @@ class tableLine:
         self.counter += 1
     def setTag(self, newTag):
         self.tag = newTag
-
+#------------------------------------------------Direct Mapped Cache-------------------------------------------------------#
 class directMappedCache:
     def __init__(self, size, lineSize):
         self.size = size
@@ -42,18 +42,115 @@ class directMappedCache:
         else:
             self.table[line].setTag(tag)
             return 'Miss'
-        
-    
+#------------------------------------------------Fully Associative Fifo Cache-------------------------------------------------------#
+class fullyAssociativeCacheFifo:
+    def __init__(self, size, lineSize):
+        self.size = size
+        self.lineSize = lineSize
+        self.numberOfLines = size // lineSize
+        self.table = []
+        for i in range(self.numberOfLines):
+            self.table.append(tableLine(i, 0, None, 0))
 
+    def access(self,address):
+        binary_string = bin(int(address,16))[2:]
+        offsetSize = int(math.log(self.lineSize, 2))
+        indexSize = int(math.log(self.size, 2))
+        
+        print(binary_string)
+        
+        tag = binary_string[:(len(binary_string) - indexSize)]       
+        offset = binary_string[(len(binary_string) - offsetSize):]
+
+        tag = int(tag, 2)
+
+        for l in self.table:
+            if l.tag == tag:
+                l.hit()
+                return 'Hit'
+        for l in self.table:
+            if l.tag is None:
+                l.setTag(tag)
+                return 'Miss'
+        self.table[0].setTag(tag)
+        return 'Miss' 
+#------------------------------------------------Fully Associative Lru Cache-------------------------------------------------------#
+class fullyAssociativeCacheLru:
+    def __init__(self, size, lineSize):
+        self.size = size
+        self.lineSize = lineSize
+        self.numberOfLines = size // lineSize
+        self.table = []
+        for i in range(self.numberOfLines):
+            self.table.append(tableLine(i, 0, None, 0))
+
+    def access(self,address):
+        binary_string = bin(int(address,16))[2:]
+        offsetSize = int(math.log(self.lineSize, 2))
+        indexSize = int(math.log(self.size, 2))
+        
+        print(binary_string)
+        
+        tag = binary_string[:(len(binary_string) - indexSize)]       
+        offset = binary_string[(len(binary_string) - offsetSize):]
+
+        tag = int(tag, 2)
+
+        lowestCount = 9999999
+        lowestCountIndex = -1
+        for i in range(len(self.table)):
+            l = self.table[i]
+            if l.tag == tag:
+                l.hit()
+                return 'Hit'
+            if l.counter < lowestCount:
+                lowestCount = l.counter
+                lowestCountIndex = i
+        self.table[lowestCountIndex].setTag(tag)
+        return 'Miss' 
+#------------------------------------------------Set Associative Fifo Cache-------------------------------------------------------#
+class SetAssociativeCacheFifo:
+    def __init__(self, size, lineSize):
+        self.size = size
+        self.lineSize = lineSize
+        self.numberOfLines = size // lineSize
+        self.table = []
+        for i in range(self.numberOfLines):
+            self.table.append(tableLine(i, 0, None, 0))
+
+    def access(self,address):
+        binary_string = bin(int(address,16))[2:]
+        offsetSize = int(math.log(self.lineSize, 2))
+        indexSize = int(math.log(self.size, 2))
+        
+        print(binary_string)
+        
+        tag = binary_string[:(len(binary_string) - indexSize)]       
+        offset = binary_string[(len(binary_string) - offsetSize):]
+
+        tag = int(tag, 2)
+
+        for l in self.table:
+            if l.tag == tag:
+                l.hit()
+                return 'Hit'
+        for l in self.table:
+            if l.tag is None:
+                l.setTag(tag)
+                return 'Miss'
+        self.table[0].setTag(tag)
+        return 'Miss' 
 
 f = open('read01.trace') # Open file on read mode
 lines = f.read().splitlines() # List with stripped line-breaks
 f.close() # Close file
 
-dMC = directMappedCache(512, 64)
+dMC = fullyAssociativeCacheLru(512, 64)
 
-my_hexdata = "1fffff78"
-dMC.access(my_hexdata)
+for line in lines:
+    lineSplit = line.split()
+    print(dMC.access(lineSplit[1][2:]))
+
 for l in dMC.table:
     print(l.index)
     print(l.tag)
